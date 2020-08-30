a84ead9389e376cc57405da0c7c39b1abdca679a
mariomui <ykanmui@viscira.com>
2020-08-29 22:25:02 -0700

    Working version of webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
-const srcPath = 'client/app/src';
+const srcPath = 'client/app/src/views';
 const distPath = 'client/app/dist';
 const entryFileLocation = path.resolve(__dirname, srcPath, 'main.js');
 console.log(entryFileLocation);
 module.exports = {
-  entry: path.resolve(__dirname, srcPath, 'main.js'),
-  output: path.resolve(__dirname, distPath),
-  filename: '[name].bundle.js',
+  entry: './client/app/src/main.js',
+  output: {
+    filename: (pathData) => {
+      return pathData.chunk.name === 'main' ? '[name].js' : '[name]/[name].js';
+    },
+    path: path.resolve(__dirname, distPath),
+  },
 
   module: {
     rules: [
