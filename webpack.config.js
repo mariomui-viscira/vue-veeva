88d83485ff9a0401cc4e608cd38e3a689dfde757
mariomui <ykanmui@viscira.com>
2020-08-30 16:11:39 -0700

    Multiple.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,29 @@
 const path = require('path');
-const srcPath = 'client/app/src/views';
+const srcPath = 'client/app/src';
+const viewPath = 'client/app/src/views';
 const distPath = 'client/app/dist';
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const entryFileLocation = path.resolve(__dirname, srcPath, 'main.js');
+const entryFileLocation = path.resolve(__dirname, viewPath, 'main.js');
 console.log(entryFileLocation);
 module.exports = {
-  entry: './client/app/src/main.js',
-  output: {
-    filename: (pathData) => {
-      return pathData.chunk.name === 'main' ? '[name].js' : '[name]/[name].js';
-    },
-    path: path.resolve(__dirname, distPath),
+  mode: 'development',
+  entry: {
+    firstslide: path.resolve(
+      __dirname,
+      viewPath,
+      'firstslide',
+      'firstslide.js'
+    ),
   },
+  output: {
+    filename: '[name]/[name].js',
+  },
+  // output: {
+  // filename: (pathData) => {
+  //   return pathData.chunk.name === 'main' ? 'me.js' : 'you.js';
+  // },
+  // path: path.resolve(__dirname, distPath),
+  // },
 
   module: {
     rules: [
@@ -21,5 +33,12 @@ module.exports = {
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(srcPath, 'template.html'),
+      chunks: ['firstslide'],
+      filename: 'firstslide/index.html',
+    }),
+  ],
 };
